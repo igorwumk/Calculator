84be6bd70edda92df279adaecc4a6f16e33e417e
Igor Wieprzkowski <igorwieprzkowski@mat.umk.pl>
2024-11-30 23:41:56 +0100

    Added basic input handling

--- app/src/main/java/pl/igorwumk/calculator/CalculatorViewModel.kt
+++ app/src/main/java/pl/igorwumk/calculator/CalculatorViewModel.kt
@@ -7,6 +7,28 @@ class CalculatorViewModel : ViewModel() {
     val expression = mutableStateOf("0")
 
     fun append(char: String) {
-        expression.value += char
+        if(char in "123456789") {
+            if(expression.value == "0") {
+                // incoming 1-9, 0 in exp -> replace 0 with incoming value
+                expression.value = char
+            }
+            else {
+                // incoming 1-9 -> add at end of exp
+                expression.value += char
+            }
+        }
+        if(char in "0" && expression.value != "0") {
+            // incoming 0, no exclusive 0 in exp -> add 0 at end of exp
+            expression.value += char
+        }
+        if(char in "+-*/") {
+            // incoming operator
+            if(expression.value.last() in "+-*/") {
+                // remove operator on exp end if already existing
+                expression.value = expression.value.dropLast(1)
+            }
+            // put operator at end of exp
+            expression.value += char
+        }
     }
 }
