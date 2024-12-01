abf8a347404f67e0e68dd4f1098b6ae48ed6d7f8
Igor Wieprzkowski <igorwieprzkowski@mat.umk.pl>
2024-12-01 16:03:09 +0100

    Added input handling for comma

--- app/src/main/java/pl/igorwumk/calculator/CalculatorViewModel.kt
+++ app/src/main/java/pl/igorwumk/calculator/CalculatorViewModel.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 
 class CalculatorViewModel : ViewModel() {
+    private var commaPresentInNumber = false
     val expression = mutableStateOf("0")
 
     fun append(char: String) {
@@ -29,6 +30,45 @@ class CalculatorViewModel : ViewModel() {
             }
             // put operator at end of exp
             expression.value += char
+            commaPresentInNumber = false
         }
+        if(char in ".") {
+            // incoming comma
+            // process only if comma not present in number
+            if(!commaPresentInNumber) {
+                if(expression.value.last() !in "0123456789") {
+                    // add 0 at end of exp if no digit before
+                    expression.value += "0"
+                }
+                expression.value += char
+                commaPresentInNumber = true
+            }
+        }
+    }
+
+    fun removeLastCharacter() {
+        if(expression.value != "0") {
+            //replace digit with 0 if it's the only digit
+            if(expression.value.length == 1) {
+                expression.value = "0"
+            }
+            else {
+                //reset comma present status when removing it
+                if(expression.value.last() == '.') {
+                    commaPresentInNumber = false
+                }
+                //remove character
+                expression.value = expression.value.dropLast(1)
+            }
+        }
+    }
+
+    fun evaluate() {
+        TODO("Not yet implemented")
+    }
+
+    fun clear() {
+        expression.value = "0"
+        commaPresentInNumber = false
     }
 }
