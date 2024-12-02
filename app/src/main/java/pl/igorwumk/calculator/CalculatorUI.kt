4bebc11d88c77bebe6f81ab53b6e71d3b305e247
Igor Wieprzkowski <igorwieprzkowski@mat.umk.pl>
2024-12-02 23:44:08 +0100

    Cleaned up unused imports

--- app/src/main/java/pl/igorwumk/calculator/CalculatorUI.kt
+++ app/src/main/java/pl/igorwumk/calculator/CalculatorUI.kt
@@ -18,12 +18,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.max
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun CalculatorButton(
--- app/src/main/java/pl/igorwumk/calculator/MainActivity.kt
+++ app/src/main/java/pl/igorwumk/calculator/MainActivity.kt
@@ -1,25 +1,13 @@
 package pl.igorwumk.calculator
 
 import android.os.Bundle
-import android.view.WindowInsets.Type.statusBars
-import android.view.WindowInsetsController
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.statusBars
-import androidx.compose.foundation.layout.systemBars
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.core.view.WindowInsetsControllerCompat
 import pl.igorwumk.calculator.ui.theme.CalculatorTheme
 
 class MainActivity : ComponentActivity() {
